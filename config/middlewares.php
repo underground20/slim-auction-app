f63e4c9cf6d01715e6d3cd082b5782d2982a4ede
Nikolay Parshakov <prognik97@gmail.com>
2022-03-12 15:14:34 +0300

    Add middleware Flusher (entity update)

--- config/middlewares.php
+++ config/middlewares.php
@@ -2,12 +2,16 @@
 
 declare(strict_types=1);
 
+use App\Http\Middleware\Flusher;
+use Doctrine\ORM\EntityManagerInterface;
 use Psr\Container\ContainerInterface;
 use Slim\App;
 
 return static function (App $app, ContainerInterface $container): void {
     /** @psalm-var array{debug:bool} */
     $config = $container->get('config');
+    $em = $container->get(EntityManagerInterface::class);
 
     $app->addErrorMiddleware($config['debug'], true, true);
+    $app->addMiddleware(new Flusher($em));
 };
---
+++ src/Http/Middleware/Flusher.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class Flusher implements MiddlewareInterface
+{
+    private EntityManagerInterface $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $response = $handler->handle($request);
+        $this->em->flush();
+
+        return $response;
+    }
+}
